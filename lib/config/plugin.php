b7232d4ac57071ad47ee5f28df4e7ea65c1dcb2c
Serge Rodovnichenko <sergerod@gmail.com>
2014-11-02 13:47:32 +0300

    Initial

---
+++ lib/config/plugin.php
@@ -0,0 +1,10 @@
+<?php
+return array (
+  'name' => 'Product Attachments',
+  'icon' => 'img/syrattach.gif',
+  'version' => '1.0.0',
+  'vendor' => '670917',
+  'handlers' => 
+  array (
+  ),
+);
---
+++ lib/shopSyrattachPlugin.class.php
@@ -0,0 +1,6 @@
+<?php
+
+class shopSyrattachPlugin extends shopPlugin
+{
+
+}
