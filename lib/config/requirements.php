136c68d4c6ecfc45d061ab9959f95b62009e9e6f
Serge Rodovnichenko <sergerod@gmail.com>
2021-01-03 16:44:36 +0300

    Requires PHP 7.2

---
+++ lib/config/requirements.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * @author Serge Rodovnichenko <serge@syrnik.com>
+ * @copyright Serge Rodovnichenko, 2021
+ * @license Webasyst
+ */
+return [
+    'php' => ['version' => '>=7.2.0', 'strict' => true]
+];
