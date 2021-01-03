45a522e1b80ccb915d1730b63391dd695d9fbb8f
Serge Rodovnichenko <sergerod@gmail.com>
2021-01-03 16:47:24 +0300

    Добавлены требования к приложениям

--- lib/config/requirements.php
+++ lib/config/requirements.php
@@ -5,5 +5,7 @@
  * @license Webasyst
  */
 return [
-    'php' => ['version' => '>=7.2.0', 'strict' => true]
+    'php'           => ['version' => '>=7.2.0', 'strict' => true],
+    'app.installer' => ['version' => '1.14', 'strict' => true],
+    'app.shop'      => ['version' => '8.16', 'strict' => false]
 ];
