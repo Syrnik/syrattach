97686eac202d595a3b3d8af33196ff08471751a1
Serge Rodovnichenko <sergerod@gmail.com>
2021-05-29 18:39:31 +0300

    Пофиксил показ размеров в байтах

--- js/prod_syrattach.js
+++ js/prod_syrattach.js
@@ -21,16 +21,16 @@
             return '';
         }
         if (bytes >= 1000000000) {
-            return (bytes / 1000000000).toFixed(2) + localize('GB');
+            return `${(bytes / 1000000000).toFixed(2)} ${localize('GB')}`;
         }
         if (bytes >= 1000000) {
-            return (bytes / 1000000).toFixed(2) + localize('MB');
+            return `${(bytes / 1000000).toFixed(2)} ${localize('MB')}`;
         }
 
         if (bytes > 1000)
-            return (bytes / 1000).toFixed(2) + localize('KB');
+            return `${(bytes / 1000).toFixed(2)} ${localize('KB')}`;
 
-        return bytes + localize('bytes');
+        return `${bytes} ${localize('bytes')}`;
     }
 
     const DescriptionEditor = {
