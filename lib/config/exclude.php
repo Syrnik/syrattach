ade4a4f3590acac2cba2ac18f3e8ac37cf993de3
Serge Rodovnichenko <sergerod@gmail.com>
2026-05-31 19:48:43 +0300

    chore: add compress-app-plugin.php script for release CI

--- lib/config/exclude.php
+++ lib/config/exclude.php
@@ -37,5 +37,6 @@ return array(
     '.gitlab-ci.yml',
     'lib/vendors/*/ci-gitlab/*',
     'lib/vendors/bin',
-    'lib/vendors/jms/metadata/tests/Metadata/Tests'
+    'lib/vendors/jms/metadata/tests/Metadata/Tests',
+    'compress-app-plugin.php',
 );
